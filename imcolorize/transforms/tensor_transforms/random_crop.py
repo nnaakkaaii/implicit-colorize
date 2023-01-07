4b42fc6f83b5dc678460664d5de17858618a2bdd
nakai-yu <nakai-yu623@g.ecc.u-tokyo.ac.jp>
2023-01-07 18:46:35 +0900

    fix bug

--- imcolorize/transforms/tensor_transforms/random_crop.py
+++ imcolorize/transforms/tensor_transforms/random_crop.py
@@ -10,8 +10,8 @@ from .interface import Interface
 class RandomCrop(Interface):
     def forward(self, x: Tuple[Tensor, Tensor]) -> Tuple[Tensor, Tensor]:
         bw, rgb = x
-        w_bw, h_bw = bw.size
-        w_rgb, h_rgb = rgb.size
+        w_bw, h_bw = bw.size()
+        w_rgb, h_rgb = rgb.size()
         assert w_bw == h_bw == w_rgb == h_rgb
         p = transforms.RandomCrop.get_params(bw, output_size=(w_bw, h_bw))
         return tf.crop(bw, *p), tf.crop(rgb, *p)
