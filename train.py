72b5b50b662b9934880fd132e31224a9941c1e17
nakai-yu <nakai-yu623@g.ecc.u-tokyo.ac.jp>
2023-01-08 00:32:49 +0900

    update default batch_size

--- train.py
+++ train.py
@@ -137,7 +137,7 @@ if __name__ == "__main__":
                         )
     parser.add_argument("--batch_size",
                         type=int,
-                        default=4,
+                        default=16,
                         )
     parser.add_argument("--num_epochs",
                         type=int,
