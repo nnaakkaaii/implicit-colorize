6dfcdc2612a76a66fb9ccd5caa9fd0c111f3a979
nakai-yu <nakai-yu623@g.ecc.u-tokyo.ac.jp>
2023-01-07 19:10:43 +0900

    fix lint

--- setup.py
+++ setup.py
@@ -2,8 +2,14 @@ from pathlib import Path
 
 from setuptools import setup
 
+
+def read_requirements():
+    with open(Path(__file__).with_name("requirements.txt")) as f:
+        return f.read().splitlines()
+
+
 setup(
     name="imcolorize",
     version="0.0.0",
-    install_requires=open(Path(__file__).with_name("requirements.txt")).read().splitlines(),
+    install_requires=read_requirements(),
 )
