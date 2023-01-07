f5a903bbadba48d9b1a9d530ed8e71ff91bf3e52
nakai-yu <nakai-yu623@g.ecc.u-tokyo.ac.jp>
2023-01-07 18:22:57 +0900

    delete duplicate type annotation

--- imcolorize/datasets/type_transforms/with_bw.py
+++ imcolorize/datasets/type_transforms/with_bw.py
@@ -8,6 +8,5 @@ from ...transforms.interface import Interface
 class WithBW(Interface):
     def forward(self, x: Tuple[Image, int]) -> Tuple[Image, Image]:
         rgb, _ = x
-        rgb: Image
         bw = rgb.convert('1')
         return bw, rgb
