9f30643bcac38004ed0262dd4e2214d6dd4698a5
nakai-yu <nakai-yu623@g.ecc.u-tokyo.ac.jp>
2023-01-08 18:18:56 +0900

    update imnet to freeze encoder

--- imcolorize/models/imnet.py
+++ imcolorize/models/imnet.py
@@ -1,4 +1,8 @@
-from torch import Tensor, nn
+from pathlib import Path
+
+import torch
+from torch import Tensor, nn, optim
+from torch.nn import DataParallel
 
 from ..networks.imnet_decoder import IMNetDecoder
 from ..networks.resnet_encoder import ResNetEncoder
@@ -6,6 +10,7 @@ from .encoder_decoder_wrapper import EncoderDecoderWrapper
 
 
 class IMNet(EncoderDecoderWrapper):
+    """IMNet freezes encoder & trains decoder"""
     encoder_filename = "net_resnet_encoder.pth"
     decoder_filename = "net_imnet_decoder.pth"
 
@@ -15,8 +20,27 @@ class IMNet(EncoderDecoderWrapper):
         self.decoder = IMNetDecoder()
         self.criterion = nn.MSELoss()
 
+        self.encoder.eval()
+
+    def save_state_dict(self, save_dir: Path) -> None:
+        net_decoder_path = save_dir / self.decoder_filename
+        if isinstance(self.decoder, DataParallel):
+            torch.save(self.decoder.module.state_dict(), net_decoder_path)
+        else:
+            torch.save(self.decoder.state_dict(), net_decoder_path)
+
     def loss(self, t: Tensor) -> Tensor:
         return self.criterion(self.y, t.to(self.device))
+
+    def forward(self, x: Tensor) -> Tensor:
+        with torch.no_grad():
+            h = self.encoder(x.to(self.device))
+        y = self.decoder(h)
+
+        self.x = x
+        self.y = y
+
+        return y
 
     def backward(self, t: Tensor) -> float:
         self.optimizer.zero_grad()
@@ -24,3 +48,16 @@ class IMNet(EncoderDecoderWrapper):
         loss.backward()
         self.optimizer.step()
         return loss.item()
+
+    def set_optim(self, **kwargs) -> None:
+        assert "decoder_lr" in kwargs
+        self.optimizer = optim.Adam(
+            self.decoder.parameters(),
+            lr=kwargs["decoder_lr"],
+            )
+
+    def train(self) -> None:
+        self.decoder.train()
+
+    def eval(self) -> None:
+        self.decoder.eval()
