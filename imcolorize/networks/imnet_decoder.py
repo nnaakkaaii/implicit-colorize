fix gradient vanishing problem

imcolorize/networks/imnet_decoder.py
@@ -30,7 +30,7 @@ class IMNetDecoder(nn.Module):
             nn.Linear(256, 128),
             nn.ReLU(),
             nn.Linear(128, 3),
-            nn.Sigmoid(),
+            nn.Tanh(),
             )
         self.img_size = img_size
 